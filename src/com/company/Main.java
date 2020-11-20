87dc29e7dfff228fedc13396efaa4fd1c38f1fdc
Rogue <lucianomirandax9@gmail.com>
2020-11-20 14:20:08 -0300

    Primer commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String[] asistencia = new String[]{
+                "Santiago",
+                "Luciano"
+        };
+
+        for(String alumnos : asistencia){
+            System.out.println(alumnos);
+        }
+
+
+
+    }
+}
